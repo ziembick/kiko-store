3bafe2bbe3b52494ec056ee952bc0c49ac43698f
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-17 21:41:20 -0300

    added redis uncomment lines

--- medusa-config.js
+++ medusa-config.js
@@ -55,7 +55,7 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -66,7 +66,7 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -78,7 +78,7 @@ const projectConfig = {
   worker_mode: process.env.MEDUSA_WORKER_MODE,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+  redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
