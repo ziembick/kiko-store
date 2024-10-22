9d75dcf08d22205f4fb9a1b6a8d9c2a65c241b34
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-21 21:58:39 -0300

    feat:angolia

--- medusa-config.js
+++ medusa-config.js
@@ -43,6 +43,33 @@ const plugins = [
     },
   },
   {
+    resolve: `medusa-plugin-algolia`,
+    options: {
+      applicationId: process.env.ALGOLIA_APP_ID,
+      adminApiKey: process.env.ALGOLIA_ADMIN_API_KEY,
+      settings: {
+        products: {
+          indexSettings: {
+            searchableAttributes: ["title", "description"],
+            attributesToRetrieve: [
+              "id",
+              "title",
+              "description",
+              "handle",
+              "thumbnail",
+              "variants",
+              "variant_sku",
+              "options",
+              "collection_title",
+              "collection_handle",
+              "images",
+            ]
+          }
+        }
+      },
+    },
+  },
+  {
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
