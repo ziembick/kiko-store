749da88eb99e4cb3713301b11dabbedd0c842d94
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-23 23:05:36 -0300

    feat: added stripe

--- medusa-config.js
+++ medusa-config.js
@@ -43,6 +43,13 @@ const plugins = [
     },
   },
   {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
+  {
     resolve: `medusa-plugin-algolia`,
     options: {
       applicationId: process.env.ALGOLIA_APP_ID,
