7b98454720373c82d99306e326a352ceb257669d
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-24 21:34:33 -0300

    sendgrid

--- medusa-config.js
+++ medusa-config.js
@@ -43,6 +43,21 @@ const plugins = [
     },
   },
   {
+    resolve: `medusa-plugin-sendgrid`,
+    options: {
+      api_key: process.env.SENDGRID_API_KEY,
+      from: process.env.SENDGRID_FROM,
+      order_placed_template: 
+        process.env.SENDGRID_ORDER_PLACED_ID,
+      localization: {
+        "de-DE": { // locale key
+          order_placed_template:
+            process.env.SENDGRID_ORDER_PLACED_ID_LOCALIZED,
+        },
+      },
+    },
+  },
+  {
     resolve: `medusa-payment-stripe`,
     options: {
       api_key: process.env.STRIPE_API_KEY,
