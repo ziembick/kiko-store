2872e39e5e02a8c2a1d278c02f290af20323bf1a
Paulo Ziembick <pauloziembick@gmail.com>
2024-11-04 23:32:28 -0300

    feat sandbox

--- medusa-config.js
+++ medusa-config.js
@@ -113,6 +113,7 @@ const plugins = [
       access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
       success_backurl: process.env.MERCADOPAGO_SUCCESS_BACKURL,
       webhook_url: process.env.MERCADOPAGO_WEBHOOK_URL,
+      sandbox: true, // false para prod
     },
   },
   
